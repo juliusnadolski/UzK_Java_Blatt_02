22f67a2d48357d60b714c8e30ce13f36b6c3388b
juliusnadolski <julius.nadolski.uni@gmail.com>
2025-10-29 14:10:24 +0100

    third_push

---
+++ p3/P3_main.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class P3_main {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 10;
+        int k = 15;
+
+        if (j>i) {
+            System.out.println("A");
+        }
+        if (k>i) {
+            System.out.println("B");
+        }
+        if (k>j) {
+            System.out.println("C");
+        }
+    }
+}
