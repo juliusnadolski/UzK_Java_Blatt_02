8d5e0bc44976749ecea78d668f82740fb853a818
juliusnadolski <julius.nadolski.uni@gmail.com>
2025-10-29 14:02:30 +0100

    second_push

---
+++ p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+
+        int i = 1;
+        boolean b = true;
+        
+        if (i > 10) {
+            b = true;
+        }
+
+        System.out.println("Value of b: " + b);
+    }
+}
